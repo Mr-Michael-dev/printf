36082e791d418137ead560e71f710c950291c154
GODSPE1 <godspy18@gmail.com>
2023-10-16 10:05:40 -0700

    paser file for the format hanlers

--- my_parser.c
+++ my_parser.c
@@ -1,4 +1,37 @@
 #include "main.h"
-int handle()
+my_parser(const char *format , va_list args)
 {
+	int len_count;
+	for (const char *format = format; *format; format++)
+	{
+		if (format == NULL)
+			return (-1);
+		if (*format != '%')
+		{
+			_putchar(*format);
+			len_count++;
+		}
+		else 
+			format++;
+		if (*format == '\0')
+			break;
+		if (*formatt == '%')
+		{
+			_putchar('%');
+			len_count++;
+		}
+		else if (*format == 'c')
+		{
+			char c = va_arg(args, char);
+			handle_char(c);
+			len_count++;
+		}
+		else if (*format == 's')
+		{
+			char *str;
+			str = va_arg(args, char *);
+			handle_string(str);
+			len_count++;
+		}
+	}
 }
