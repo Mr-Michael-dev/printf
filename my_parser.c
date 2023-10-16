bfcaef7334f2e3b809aecb661235ee4c9cb6870b
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-10-16 11:01:04 -0700

    debug my_parser

--- my_parser.c
+++ my_parser.c
@@ -1,44 +1,56 @@
 #include "main.h"
-my_parser(const char *format, va_list args)
-{
-	int len_count;
+int my_parser(const char *format, va_list args);
+/**
+ * my_parser - parse the format string and check for format specifiers
+ * and print arguments passed to the function
+ * @format: The format string
+ * @args: arguments passed from _printf
+ *
+ * Return: total length printed
+ */
 
-	for (const char *format = format; *format; format++)
+int my_parser(const char *format, va_list args)
+{
+	int len_count = 0;
+	const char *p;
+	char c;
+	char *str;
+	int integer;
+
+	for (p = format; *format; p++)
 	{
-		if (format == NULL)
+		if (*p == NULL)
 			return (-1);
-		if (*format != '%')
+		if (*p != '%')
 		{
-			_putchar(*format);
+			_putchar(*p);
 			len_count++;
 		}
 		else
-			format++;
-		if (*format == '\0')
+			p++;
+		if (*p == '\0')
 			break;
-		if (*formatt == '%')
+		if (*p == '%')
 		{
 			_putchar('%');
 			len_count++;
 		}
-		else if (*format == 'c')
+		else if (*p == 'c')
 		{
-			char c = va_arg(args, char);
+			c = va_arg(args, char);
 
 			handle_char(c);
 			len_count++;
 		}
-		else if (*format == 's')
+		else if (*p == 's')
 		{
-			char *str;
-
 			str = va_arg(args, char *);
 			handle_string(str);
 			len_count++;
 		}
-		else if (*format == 'd')
+		else if (*p == 'd')
 		{
-			int integer = va_arg(args, int);
+			integer = va_arg(args, int);
 
 			handle_int(integer);
 			len_count++;
