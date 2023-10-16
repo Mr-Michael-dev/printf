957fb469d8e04a830b73ee76a9e0458f8c382321
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-10-16 07:38:24 -0700

    create _putchar.c and handle_char.c

---
+++ handle_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * handle_char - prints a single character to stdout
+ * @arg: character to be printed
+ *
+ * Return: 0
+ */
+
+int handle_char(char arg)
+{
+	_putchar(arg);
+
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 
+int _putchar(char c);
 
+int handle_char(char arg);
+
+int handle_str(char *arg);
+
+int handle_int(int arg);
 
 #endif
